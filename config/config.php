c0ce238164b5697c828a5eb954dbc39a17103f99
Nadeerad <ynadeerad@gmail.com>
2025-04-01 20:31:59 +0000

    Remove API key code

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'openai_api_key' => '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',
+    'openai_api_key' => '',
 ];
